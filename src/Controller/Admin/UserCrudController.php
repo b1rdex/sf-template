373325191f4ef23c7dd323a1022d6e4bd8359650
Anatoly Pashin <anatoly.pashin@gmail.com>
2022-12-06 20:57:48 +0700

    fix users crud

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,23 +3,81 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+    public function __construct(private readonly UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+        yield IdField::new('id')->hideOnForm();
+
+        yield from [
+            TextField::new('username'),
         ];
+
+        $roles = [
+            'Admin' => 'ROLE_ADMIN',
+        ];
+        yield ChoiceField::new('roles')
+            ->allowMultipleChoices()
+            ->setChoices($roles)
+            ->setRequired(false);
+
+        yield TextField::new('password')->hideOnIndex();
     }
-    */
+
+    public function createEditFormBuilder(
+        EntityDto $entityDto,
+        KeyValueStore $formOptions,
+        AdminContext $context
+    ): FormBuilderInterface {
+        $plainPassword = $entityDto->getInstance()?->getPassword();
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+        $this->addEncodePasswordEventListener($formBuilder, $plainPassword);
+
+        return $formBuilder;
+    }
+
+    private function addEncodePasswordEventListener(
+        FormBuilderInterface $formBuilder,
+        string $plainPassword = null
+    ): void {
+        $formBuilder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) use ($plainPassword) {
+            /** @var User $user */
+            $user = $event->getData();
+            if ($user->getPassword() !== $plainPassword) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+            }
+        });
+    }
+
+    public function createNewFormBuilder(
+        EntityDto $entityDto,
+        KeyValueStore $formOptions,
+        AdminContext $context
+    ): FormBuilderInterface {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+        $this->addEncodePasswordEventListener($formBuilder);
+
+        return $formBuilder;
+    }
 }
