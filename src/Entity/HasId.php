ba2f816a7d3f45c5d2acb703258007e62316a0bf
Anatoly Pashin <anatoly.pashin@gmail.com>
2022-12-07 14:48:01 +0700

    update

---
+++ src/Entity/HasId.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UlidType;
+use Symfony\Component\Uid\Ulid;
+
+/**
+ *
+ */
+trait HasId
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UlidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.ulid_generator')]
+    private Ulid $id;
+
+    public function getId(): Ulid
+    {
+        return $this->id;
+    }
+}
